f89fcda4c9aa875af405bc6eb03e2bbcd82d054d
ABHISHEK RAJ <rajtheinnovatorabhishek@gmail.com>
2016-09-30 23:48:32 +0530

    added basic search layout

--- app/src/main/java/com/example/android/newstoday/MainActivity.java
+++ app/src/main/java/com/example/android/newstoday/MainActivity.java
@@ -1,34 +1,61 @@
 package com.example.android.newstoday;
 
+import android.app.LoaderManager;
 import android.app.SearchManager;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static com.example.android.newstoday.R.id.search;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String GUARDIAN_API_URL = "http://content.guardianapis.com/search?q=";
+    private static final int NEWS_LOADER_ID = 1;
+    // display error
+    //  TextView mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    //Add the menu item(s)
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.options_menu, menu);
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.searchBoxAfterClick);
+        linearLayout.setVisibility(View.GONE);
+        Button searchButtonBeforeFirstSearch = (Button) findViewById(R.id.buttonBeforeSearch);
+        //Set click Listener on Search Button Click
+        searchButtonBeforeFirstSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Find the edit text's actual text and make it compatible for a url search query
+                String searchQuery = ((EditText) findViewById(R.id.editTextBeforeClick)).getText().toString().replace(" ", "%20");
 
-        // Associate searchable configuration with the SearchView
-        SearchManager searchManager =
-                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView =
-                (SearchView) menu.findItem(R.id.search).getActionView();
-        searchView.setSearchableInfo(
-                searchManager.getSearchableInfo(getComponentName()));
-
-        return true;
+                //Check if user input is empty or it contains some query text
+                if (searchQuery.isEmpty()) {
+                    Context context = getApplicationContext();
+                    String text = "Nothing Entered in Search";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                } else {
+                    LinearLayout searchBoxBeforeClick = (LinearLayout) findViewById(R.id.searchBoxBeforeClick);
+                    searchBoxBeforeClick.setVisibility(View.GONE);
+                    LinearLayout searchBoxAfterClick = (LinearLayout) findViewById(R.id.searchBoxAfterClick);
+                    searchBoxAfterClick.setVisibility(View.VISIBLE);
+                    TextView searchQueriedFor = (TextView) findViewById(R.id.searchQueriedFor);
+                    searchQueriedFor.setText(searchQuery);
+                }
+            }
+        });
     }
 }
