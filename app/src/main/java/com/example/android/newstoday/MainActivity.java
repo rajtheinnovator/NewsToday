cf8516bd38a48cba885f0aab404ff30850d25c17
ABHISHEK RAJ <rajtheinnovatorabhishek@gmail.com>
2016-10-03 23:36:23 +0530

    not able to handle second button click

--- app/src/main/java/com/example/android/newstoday/MainActivity.java
+++ app/src/main/java/com/example/android/newstoday/MainActivity.java
@@ -19,17 +19,13 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
 import java.util.ArrayList;
 
-
 import static com.example.android.newstoday.NewsLoader.LOG_TAG;
-import static com.example.android.newstoday.R.id.news_list_view;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<ArrayList<News>> {
     private static final String GUARDIAN_API_URL = "http://content.guardianapis.com/search?";
     private static final int NEWS_LOADER_ID = 1;
-    private static final int NEWS_LOADER_ID_2 = 2;
     String searchQuery;
     // display error
     //  TextView mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
@@ -60,34 +56,20 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
                     searchBoxBeforeClick.setVisibility(View.GONE);
                     LinearLayout searchBoxAfterClick = (LinearLayout) findViewById(R.id.searchBoxAfterClick);
                     searchBoxAfterClick.setVisibility(View.VISIBLE);
-                    TextView searchQueriedFor = (TextView) findViewById(R.id.searchQueriedFor);
-                    searchQueriedFor.setText(searchQueried);
-                    searchQuery = searchQueried.replace(" ", "%20");
-                    //First of all check if network is connected or not then only start the laoder
-                    ConnectivityManager connMgr = (ConnectivityManager)
-                            getSystemService(Context.CONNECTIVITY_SERVICE);
-                    NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-                    if (networkInfo != null && networkInfo.isConnected()) {
-            /*
-            fetch data
-            Get a reference to the LoaderManager, in order to interact with loaders.
-            And, Initialize the loader. Pass in the int ID constant defined above and pass in null for
-            the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
-            because this activity implements the LoaderCallbacks interface).
-        */
-                        myLoaderManager();
-                    } else {
-                        // display error
-                    }
+                    //Handle the loader manager as per the the button and view selected
+                    clickHandle(searchQueried);
                 }
             }
         });
-        Button searchButtonAfterClick = (Button) findViewById(R.id.buttonAfterSearch);
-        searchButtonAfterClick.setOnClickListener(new View.OnClickListener() {
+        /*
+        Handle the Button click after result has been fetched after the sfirst click, with the
+        layout that was present on app start
+         */
+        Button searchButtonAfterFirstSearch = (Button) findViewById(R.id.buttonAfterSearch);
+        //Set click Listener on Search Button Click
+        searchButtonAfterFirstSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                newsAdapter.clear();
-//                news_list_view.setVisibility(View.INVISIBLE);
                 //Find the edit text's actual text and make it compatible for a url search query
                 String searchQueried = ((EditText) findViewById(R.id.editTextAfterClick)).getText().toString();
 
@@ -99,14 +81,22 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
                     Toast toast = Toast.makeText(context, text, duration);
                     toast.show();
                 } else {
-                    TextView searchQueriedFor = (TextView) findViewById(R.id.searchQueriedFor);
-                    searchQueriedFor.setText(searchQueried);
-                    searchQuery = searchQueried.replace(" ", "%20");
-                    //First of all check if network is connected or not then only start the laoder
-                    ConnectivityManager connMgr = (ConnectivityManager)
-                            getSystemService(Context.CONNECTIVITY_SERVICE);
-                    NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-                    if (networkInfo != null && networkInfo.isConnected()) {
+                    //Handle the loader manager as per the the button and view selected
+                    clickHandle(searchQueried);
+                }
+            }
+        });
+    }
+
+    private void clickHandle(String searchQueried) {
+        TextView searchQueriedFor = (TextView) findViewById(R.id.searchQueriedFor);
+        searchQueriedFor.setText(searchQueried);
+        searchQuery = searchQueried.replace(" ", "%20");
+        //First of all check if network is connected or not then only start the laoder
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
             /*
             fetch data
             Get a reference to the LoaderManager, in order to interact with loaders.
@@ -114,22 +104,15 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
             because this activity implements the LoaderCallbacks interface).
         */
-                        myLoaderManager1();
-                    } else {
-                        // display error
-                    }
-                }
-            }
-        });
+            myLoaderManager();
+        } else {
+            // display error
+        }
     }
 
     private void myLoaderManager() {
         LoaderManager loaderManager = getLoaderManager();
         loaderManager.initLoader(NEWS_LOADER_ID, null, this);
-    }
-    private void myLoaderManager1() {
-        LoaderManager loaderManager = getLoaderManager();
-        loaderManager.initLoader(NEWS_LOADER_ID_2, null, this);
     }
 
     @Override
@@ -150,10 +133,10 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     public void onLoadFinished(Loader<ArrayList<News>> loader, final ArrayList<News> newses) {
 
         // Create a new {@link ArrayAdapter} of earthquakes
-        final NewsAdapter newsAdapter = new NewsAdapter(MainActivity.this, newses);
+        NewsAdapter newsAdapter = new NewsAdapter(MainActivity.this, newses);
 
         // Find a reference to the {@link ListView} in the layout
-        final ListView news_list_view = (ListView) findViewById(news_list_view);
+        ListView news_list_view = (ListView) findViewById(R.id.news_list_view);
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
         news_list_view.setAdapter(newsAdapter);
@@ -162,11 +145,11 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                 News news = newses.get(position);
-                Intent booksIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(news.getWebUrl()));
-                startActivity(booksIntent);
+                Intent goToUrl = new Intent(Intent.ACTION_VIEW);
+                goToUrl.setData(Uri.parse(news.getWebUrl()));
+                startActivity(goToUrl);
             }
         });
-
     }
 
     @Override
